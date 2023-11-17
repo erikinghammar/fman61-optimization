30bc4b4a168a1e3564eb814cfe86b7957a390bc2
Erik Inghammar <erikinghammar@gmail.com>
2023-11-17 10:19:00 +0100

    Small change to penalty.m

--- Lab2/penalty.m
+++ Lab2/penalty.m
@@ -4,6 +4,6 @@ function alfa = penalty(x, A, b)
 % Description:
 %   Returns the solution to alfa(x) = sum(max(0, g(x)) where g(x) := Ax - b
 %   for a vector x, matrix A 
-    alfa = sum(max(zeros(size(x)), A*x -b));
+    alfa = sum(max(0, A*x -b));
 end
 
