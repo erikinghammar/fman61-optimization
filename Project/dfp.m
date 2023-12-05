a948e5efac3b215d4f6f4751ab19575cdacc4ed9
Erik Inghammar <erikinghammar@gmail.com>
2023-12-05 15:21:15 +0100

    dfp: initial structure of the solver

---
+++ Project/dfp.m
@@ -0,0 +1,11 @@
+function [x_opt, N_eval, N_iter] = dfp(objective_func,x0, tol, restart, printout)
+%DFP Summary of this function goes here
+%   Detailed explanation goes here
+%   TODO: write docstring
+
+
+x_opt = x0;
+N_eval = 1;
+N_iter = 1;
+end
+
