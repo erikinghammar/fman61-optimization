f6f29c78285d88c0eb90ad613cdf5085b086f493
Erik Inghammar <erikinghammar@gmail.com>
2023-12-06 11:44:02 +0100

    printout: create new print function

---
+++ Project/print_out.m
@@ -0,0 +1,14 @@
+function print_out(N_itr, x_itr, fx, n_grad, ls_fun_evals, lambda)
+% PRINT_ITR Print the current iteration level in the form specified in the
+% manual.
+%
+%   print_itr(N_itr, x_itr, fx, n_grad, ls_fun_evals, lambda)
+%
+% INPUTS:
+%   N_itr       : the current iteration level.
+%   x_itr       : the current x vector.
+%   fx          : the value of the function at the point x.
+%   n_grad      : the norm of the gradient of the function f at point x.
+%   ls_fun_eval : ?
+%   
+end
